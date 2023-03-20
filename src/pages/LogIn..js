45822c02c0ec87d948657a5aebff8a2fdccdba29
Zain Ali Khokhar <zainalikhokhar40@gmail.com>
2023-03-20 16:46:07 +0500

    login page 2

--- src/pages/LogIn..js
+++ src/pages/LogIn..js
@@ -8,19 +8,21 @@ export default function LandingPage() {
     return (
         <div className="main_box">
             <div className="form_box">
-                
+                <h3>Login to Medinav</h3>
                 <form>
-                    <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    <div className="mb-3 form_row">
+                        <label for="exampleInputEmail1" className="form-label">Contact Number</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" placeholder="03XXXXXXXXX" />
+                        {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
                     </div>
-                    <div className="mb-3">
+                    <div className="mb-3 form_row">
                         <label for="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Enter Your Password" />
                     </div>
-                    
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <div className="bring_child_to_center">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+
                 </form>
             </div>
 
