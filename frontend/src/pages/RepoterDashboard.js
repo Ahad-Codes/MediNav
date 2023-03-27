89de4375b301d3aac4381d6ade0fafe76c202b05
KhuzemahHQazi <118563782+KhuzemahHQazi@users.noreply.github.com>
2023-03-27 20:03:49 +0500

    Update RepoterDashboard.js

--- frontend/src/pages/RepoterDashboard.js
+++ frontend/src/pages/RepoterDashboard.js
@@ -1,5 +1,7 @@
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import {useNavigate} from 'react-router-dom';
+
 import "../css/TempDashboard.css"
 
 export default function TempDashboard() {
@@ -23,11 +25,40 @@ export default function TempDashboard() {
 }
 
 function Map() {
+
+  const navigate = useNavigate();
+
+  const onViewBroadcastClick = () => {
+    navigate('ViewBroadcasts');
+  }
+
+  const onViewHospitalClick = () => {
+    navigate('ViewHospitals');
+  }
   
-    console.log("Loaded")
+  console.log("Loaded")
   return (
-    <GoogleMap zoom={14} center={{ lat:31.465001, lng:74.393663}} mapContainerClassName="map-container"/>
-      
+    <div>
+      <div>
+        <GoogleMap zoom={14} center={{ lat:31.465001, lng:74.393663}} mapContainerClassName="map-container"/>
+      </div>
+    
+    <p>
+    
+        <button className="button" onClick={onViewBroadcastClick}>
+            <h1>View Broadcasts</h1>
+        </button>
+        
+        <button className="button">
+          <h1>Report Incident</h1>
+        </button>
+
+        <button className="button" onClick={onViewHospitalClick}>
+          <h1>View Nearest Hospitals</h1>
+        </button>
+    </p>
+    </div>
+
 
   );
 }
