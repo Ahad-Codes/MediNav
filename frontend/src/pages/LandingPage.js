ffc3c31bf0ed0658bf773ee702bfd8b52ec7948e
KhuzemahHQazi <118563782+KhuzemahHQazi@users.noreply.github.com>
2023-04-04 17:30:45 +0500

    Navigating from button to login

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -10,7 +10,7 @@ export default function LandingPage() {
   const navigate = useNavigate();
 
   const onReportClick = () => {
-    navigate("reporter/ReportAccident");
+    navigate("Login");
   };
 
   return (
